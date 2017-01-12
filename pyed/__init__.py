d14f76ed0eebd75750b9267b20d99f5c3a08e303
Jens Wilberg <jens_wilberg@outlook.com>
2017-01-12 12:15:48 +0100

    Update __init__.py
    Changed the __version__ variable

--- pyed/__init__.py
+++ pyed/__init__.py
@@ -1,5 +1,4 @@
 # flake8: noqa: F401
 """Python package initialization."""
 from pyed.__main__ import main
-from pyed.__version__ import VERSION_STRING
-__version__ = VERSION_STRING
+from pyed.__version__ import VERSION_STRING as __version__
